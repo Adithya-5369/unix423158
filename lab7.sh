c88f22489a6519903cd13a99c3f3eed118284ff3
Adithya <muttaadithyasaisrinivas@gmail.com>
2025-04-08 13:15:01 +0530

    Lab7 Commit

---
+++ lab7.sh
@@ -0,0 +1,18 @@
+#q1
+sudo apt install git
+#q2
+git config --global user.name "Adithya"
+git config --global user.email "muttaadithyasaisrinivas@gmail.com"
+#q3
+mkdir myproj
+cd myproj
+git init
+#q4
+touch new.sh
+git add .
+git commit -m "Initial Commit"
+#q5
+git remote add orgin https://github.com/Adithya-5369/unix423158.git
+#q6
+git branch -M main
+git push -u origin main
